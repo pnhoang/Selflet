db80008aa6703edfffd6aafe8191fbc5c67842f8
Hoang Pham <pnhoang@gmail.com>
2011-11-24 17:50:54 +0100

    Enable saving .jar file into "Abilities" folder

--- SelfletBehavior.diagram/src/selfletbehavior/diagram/sheet/CustomisedPropertyDescriptor.java
+++ SelfletBehavior.diagram/src/selfletbehavior/diagram/sheet/CustomisedPropertyDescriptor.java
@@ -1,5 +1,17 @@
 package selfletbehavior.diagram.sheet;
 
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.jar.JarFile;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.CommonPlugin;
 import org.eclipse.emf.common.ui.celleditor.ExtendedDialogCellEditor;
 import org.eclipse.emf.common.util.URI;
@@ -9,13 +21,20 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.ui.provider.PropertyDescriptor;
+import org.eclipse.jface.text.ITextSelection;
 import org.eclipse.jface.viewers.CellEditor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.internal.Workbench;
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
 
 import selfletbehavior.SelfletbehaviorPackage;
@@ -52,11 +71,14 @@ public class CustomisedPropertyDescriptor extends PropertyDescriptor implements
 					Shell shell = PlatformUI.getWorkbench().getDisplay().getActiveShell();
 					FileDialog dialog = new FileDialog(shell, SWT.OPEN);
 					
+					dialog.setFilterNames(new String[] {".jar files"});
+					dialog.setFilterExtensions(new String[] {"*.jar"});
+					
 					//set the default path
 					URI uri = ((EObject) object).eResource().getURI();
 					URI resolved = CommonPlugin.resolve(uri);
-					resolved = resolved.trimFileExtension();
-					dialog.setFilterPath(resolved.toFileString());
+					URI resolvedTrim = resolved.trimFileExtension();
+					dialog.setFilterPath(resolvedTrim.toFileString());
 					
 					
 					//set the default file
@@ -69,16 +91,43 @@ public class CustomisedPropertyDescriptor extends PropertyDescriptor implements
 						return getValue();
 					}
 					
+					
 					//extract out the source path to make it relative
 					URI deres = URI.createFileURI(fileSelected).deresolve(resolved);
 					fileSelected = deres.toString();
+					String fileNameSelected = deres.lastSegment(); 
+					/**
+					 * HACKED ON OSX WHEN USER SELECTS A FOLDER WITH WHITESPACE! 
+					 * SHOULD CHECK WHAT HAPPENS ON WINDOWS
+					 */
+					String fileNameWhiteSpaceReplaced = fileSelected.replace("%20", " ");
+						
 					
-					return valueHandler.toValue(fileSelected);
+					
+					URI project = resolved.trimSegments(2);
+					String destFile = project + "/abilities/" + fileNameSelected;
+					String destFileNoPrefix = destFile.replace("file:", ""); 
+	                
+					
+					try {
+						/**
+						 * AFTER COPYING, THE NEW FILE DOES NOT APPEAR ON THE PROJECT FOLDER IMMEDIATELY
+						 * ENABLE AUTOMATIC WORKSPACE REFRESH WHEN SOURCE FILES CHANGED IN THE PREFERENCE
+						 */
+						FileHandler.binaryCopyFile(fileNameWhiteSpaceReplaced, destFileNoPrefix);
+						
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+					return valueHandler.toValue(destFileNoPrefix);
 				}
 			};
 			return result;
 		}
 		return super.createPropertyEditor(composite);
 	}
+	
+	
 	
 }
---
+++ SelfletBehavior.diagram/src/selfletbehavior/diagram/sheet/FileHandler.java
@@ -0,0 +1,173 @@
+package selfletbehavior.diagram.sheet;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Enumeration;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import java.io.*;
+import java.util.*;
+
+
+public class FileHandler {
+
+	public static String readTextFile(File file) {
+		 StringBuilder contents = new StringBuilder();
+		    
+		    try {
+		      //use buffering, reading one line at a time
+		      //FileReader always assumes default encoding is OK!
+		     // BufferedReader input =  new BufferedReader(new FileReader(file));
+		    	
+		    	// use iso-1 decoding
+		    	Charset charset = Charset.forName("UTF-8");
+		    	BufferedReader input =  new BufferedReader(new InputStreamReader(new FileInputStream(file), charset.newDecoder()));
+		      try {
+		        String line = null; //not declared within while loop
+		        /*
+		        * readLine is a bit quirky :
+		        * it returns the content of a line MINUS the newline.
+		        * it returns null only for the END of the stream.
+		        * it returns an empty String if two newlines appear in a row.
+		        */
+		        while (( line = input.readLine()) != null){
+		          contents.append(line);
+		          contents.append(System.getProperty("line.separator"));
+		        }
+		      }
+		      finally {
+		        input.close();
+		      }
+		    }
+		    catch (IOException ex){
+		    	
+		    }
+		    
+		    return contents.toString();
+
+	}
+	
+	
+	public static void binaryCopyFile(String strSourceFile, String strDestinationFile){
+		try
+		{
+			//create FileInputStream object for source file
+			FileInputStream fin = new FileInputStream(strSourceFile);
+ 
+			//create FileOutputStream object for destination file
+			FileOutputStream fout = new FileOutputStream(strDestinationFile);
+ 
+			byte[] b = new byte[1024];
+			int noOfBytes = 0;
+ 
+			System.out.println("Copying file using streams");
+ 
+			//read bytes from source file and write to destination file
+			while( (noOfBytes = fin.read(b)) != -1 )
+			{
+				fout.write(b, 0, noOfBytes);
+			}
+ 
+			System.out.println("File copied!");
+ 
+			//close the streams
+			fin.close();
+			fout.close();			
+ 
+		}
+		catch(FileNotFoundException fnf)
+		{
+			System.out.println("Specified file not found :" + fnf);
+		}
+		catch(IOException ioe)
+		{
+			System.out.println("Error while copying file :" + ioe);
+		}
+	}
+	 
+	   public static void copyJarFile(JarFile jarFile, File destDir) throws IOException {
+	       String fileName = jarFile.getName();
+	       String fileNameLastPart = fileName.substring(fileName.lastIndexOf(File.separator));
+	       File destFile = new File(destDir, fileNameLastPart);
+
+	       JarOutputStream jos = new JarOutputStream(new FileOutputStream(destFile));
+	       Enumeration<JarEntry> entries = jarFile.entries();
+
+	       while (entries.hasMoreElements()) {
+	           JarEntry entry = entries.nextElement();
+	           InputStream is = jarFile.getInputStream(entry);
+
+	           //jos.putNextEntry(entry);
+	           //create a new entry to avoid ZipException: invalid entry compressed size
+	           jos.putNextEntry(new JarEntry(entry.getName()));
+	           byte[] buffer = new byte[4096];
+	           int bytesRead = 0;
+	           while ((bytesRead = is.read(buffer)) != -1) {
+	               jos.write(buffer, 0, bytesRead);
+	           }
+	           is.close();
+	           jos.flush();
+	           jos.closeEntry();
+	       }
+	       jos.close();
+	   }
+	   
+	   
+	   
+	 //=============================================================== copyFile
+	    // Uses BufferedReader for file input.
+	    public static void copyFile(File fromFile, File toFile) throws IOException {
+	        BufferedReader reader = new BufferedReader(new FileReader(fromFile));
+	        BufferedWriter writer = new BufferedWriter(new FileWriter(toFile));
+
+	        //... Loop as long as there are input lines.
+	        String line = null;
+	        while ((line=reader.readLine()) != null) {
+	            writer.write(line);
+	            writer.newLine();   // Write system dependent end of line.
+	        }
+
+	        //... Close reader and writer.
+	        reader.close();  // Close to unlock.
+	        writer.close();  // Close to unlock and flush to disk.
+	    }
+
+
+	    //=============================================================== copyFile2
+	    // Uses Scanner for file input.
+	    public static void copyFile2(File fromFile, File toFile) throws IOException {
+	        Scanner freader = new Scanner(fromFile);
+	        BufferedWriter writer = new BufferedWriter(new FileWriter(toFile));
+
+	        //... Loop as long as there are input lines.
+	        String line = null;
+	        while (freader.hasNextLine()) {
+	            line = freader.nextLine();
+	            writer.write(line);
+	            writer.newLine();   // Write system dependent end of line.
+	        }
+
+	        //... Close reader and writer.
+	        freader.close();  // Close to unlock.
+	        writer.close();  // Close to unlock and flush to disk.
+	    }
+	
+}
